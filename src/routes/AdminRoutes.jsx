e764ae63d3d9c36d7c41550a4e077f4478c762b8
Dennis Wachira <wachiradenis336@gmail.com>
2025-06-25 07:46:16 +0300

    feat: update admin and client routes

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -1,16 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Routes, Route } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+// Admin Pages
 import AdminPanel from "../pages/admin/AdminPanel";
 import AdminProductListPage from "../pages/admin/AdminProductListPage";
 import AdminOrdersPage from "../pages/admin/AdminOrdersPage";
-import AdminUsersPage from "../pages/admin/AdminUsersPage"; 
 
-export default function AdminRoutes() {
+const AdminRoutes = () => {
+  const { user } = useContext(AuthContext);
+
+  if (!user || user.role !== "admin") {
+    return <Navigate to="/" replace />;
+  }
+
   return (
     <Routes>
       <Route path="/" element={<AdminPanel />} />
       <Route path="/products" element={<AdminProductListPage />} />
       <Route path="/orders" element={<AdminOrdersPage />} />
-      <Route path="/users" element={<AdminUsersPage />} />
     </Routes>
   );
-}
+};
+
+export default AdminRoutes;
--- src/routes/ClientRoutes.jsx
+++ src/routes/ClientRoutes.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/user/Home";
-import AboutUs from "../pages/user/AboutUs";
 
 import CartPage from "../pages/user/CartPage";
 
 import ProfilePage from "../pages/user/ProfilePage";
-import CheckoutPage from "../pages/user/CheckoutPage";
+
 import ProductList from "../pages/user/ProductList";
+import CheckoutPage from "../pages/user/CheckoutPage";
+import OrderSuccess from "../pages/user/OrderSuccess";
 
 import Login from "../pages/user/Login";
 import Register from "../pages/user/Register";
@@ -16,17 +17,17 @@ export default function ClientRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<AboutUs />} />
-   
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-     
-     
       
+      
+      <Route path="/cart" element={<CartPage />} />
+     
+    
+      <Route path="/checkout" element={<CheckoutPage />} />
+     <Route path="/checkout/success" element={<OrderSuccess />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/profile" element={<ProfilePage />} />
-      
+     
       <Route path="/products" element={<ProductList />} />
    
     </Routes>
