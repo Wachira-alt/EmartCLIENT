e7ebedd7963fff6e0a86b6796edc4dd9d80e60b9
Dennis Wachira <wachiradenis336@gmail.com>
2025-06-28 18:16:53 +0300

    feat: add view password on and off

--- src/components/ui/form-field.jsx
+++ src/components/ui/form-field.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Controller } from "react-hook-form";
 import { Input } from "./input";
 import { Label } from "./label";
+import { Eye, EyeOff } from "lucide-react";
 
 const FormField = ({
   name,
@@ -10,6 +12,10 @@ const FormField = ({
   placeholder,
   icon: Icon, //  receives icon component
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+  const inputType = isPassword ? (showPassword ? "text" : "password") : type;
+
   return (
     <div className="space-y-1">
       <Label htmlFor={name}>{label}</Label>
@@ -19,19 +25,33 @@ const FormField = ({
         render={({ field, fieldState }) => (
           <>
             <div className="relative">
+              {/* left icon */}
               {Icon && (
                 <span className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground">
                   <Icon size={16} />
                 </span>
               )}
+              {/* input field */}
               <Input
                 id={name}
-                type={type}
+                type={inputType}
                 placeholder={placeholder}
                 className={Icon ? "pl-9" : ""}
                 {...field}
               />
+              {/* right toogle for password */}
+              {isPassword && (
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground"
+                  tabIndex={-1}
+                >
+                  {showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
+                </button>
+              )}
             </div>
+            {/* error */}
             {fieldState.error && (
               <p className="text-sm text-red-500">{fieldState.error.message}</p>
             )}
